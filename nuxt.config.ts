4a00b6e1741490331d14b10147fc9ba8df77e495
mikhail12 <misha.romadinov@gmail.com>
2025-10-01 19:14:25 +1000

    reinitialize to get tailwind working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxt/devtools',
-    '@nuxt/eslint',
-    '@nuxt/test-utils'
-  ]
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
