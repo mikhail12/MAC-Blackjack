81767cfd637d79c966036a66cbd5bdfc10939f32
mikhail12 <misha.romadinov@gmail.com>
2025-10-01 22:25:11 +1000

    Starting desktop homepage (#1)
    * Initial design
    
    * Update the meta
    
    * clean up files a bit
    
    * update ci to node 22

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div class="h-15 items-center flex w-full justify-between px-10">
+      <div class="flex gap-5 items-center">
+        <NuxtLink to="/" class="font-bold text-3xl">Blackjack</NuxtLink>
+        <CurrentBalance />
+      </div>
+      <div class="flex gap-5 items-center">
+        <NuxtLink to="/">History</NuxtLink>
+        <button>Login</button>
+        <DarkModePicker />
+      </div>
+    </div>
+    <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+import { Button } from "@/components/ui/button"
+const colorMode = useColorMode();
+const isDarkMode = ref(false);
+watchEffect(() => {
+  colorMode.preference = isDarkMode.value ? "dark" : "light";
+});
+</script>
---
+++ app/components/DarkModePicker.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Button @click="isDarkMode=!isDarkMode" variant="outline" class="h-[2.4rem] w-fit">
+        <Icon name="radix-icons:sun" class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Icon name="radix-icons:moon" class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <span class="sr-only">Toggle theme</span>
+    </Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Button } from "@/components/ui/button"
+
+const colorMode = useColorMode();
+const isDarkMode = ref(false);
+
+watchEffect(() => {
+  colorMode.preference = isDarkMode.value ? "dark" : "light";
+});
+</script>
