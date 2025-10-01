e89f4586b9bd3f5d28d3ab7b6e8729860506368e
mikhail12 <misha.romadinov@gmail.com>
2025-10-01 22:20:32 +1000

    Update the meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Blackjack',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/spade-card.ico' },
+      ],
+    },
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
