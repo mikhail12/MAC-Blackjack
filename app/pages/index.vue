0352d67f3091204703f65439ca578cadc6b79d9b
mikhail12 <misha.romadinov@gmail.com>
2025-10-03 19:08:43 +1000

    Initial connection

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>
+        Hey! {{ data?.data }} aa
+    </h1>
+</template>
+
+<script setup lang="ts">
+import type { Database } from '#build/types/supabase-database';
+
+const client = useSupabaseClient<Database>();
+
+const data = await useAsyncData('testtable', async () => {
+    const data = await client.from('testtable').select();
+    return data;
+});
+</script>
