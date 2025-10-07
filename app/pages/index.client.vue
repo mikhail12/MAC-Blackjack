81e64123beeeef0482d8c11904ef2bf5989bc55b
mikhail12 <misha.romadinov@gmail.com>
2025-10-07 19:31:30 +1100

    implement copilot changes

--- app/pages/index.client.vue
+++ app/pages/index.client.vue
@@ -6,13 +6,13 @@
                     <div class="flex justify-center gap-2">
                         <div
                             v-for="card in game.currentGame.value?.dealerHand"
-                            :key="card.rank"
+                            :key="card.cardNumber"
                             class="flex w-20 h-30 dark:bg-white bg-secondary rounded-xl"
                         >
                             <BlackjackCard :card="card" />
                         </div>
                     </div>
-                    <div :class="`px-4 py-1 text-md font-medium mx-auto rounded-[10px] flex ${dealerStatusClasses}`">
+                    <div :class="`px-4 py-1 text-md font-medium mx-auto rounded-[10px] flex bg-secondary text-primary`">
                         <span class="mr-2 text-xl">{{ game.dealerTotal }}</span>
                         <span class="my-auto">Dealer</span>
                     </div>
@@ -21,7 +21,7 @@
                     <div class="flex justify-center gap-2">
                         <div
                             v-for="card in game.currentGame.value?.playerHand"
-                            :key="card.rank"
+                            :key="card.cardNumber"
                             class="flex w-20 h-30 dark:bg-white bg-secondary rounded-xl"
                         >
                             <BlackjackCard :card="card" />
@@ -38,7 +38,7 @@
                 <Button v-if="game.canHitOrStand.value" @click="game.stand()">Stand</Button>
                 <Button v-if="game.canReset.value" @click="reset">New game</Button>
                 <div v-if="game.gameStatus.value == GameStatus.idle" class="flex flex-col items-center gap-4">
-                    <input v-model="newBet" class="bg-secondary text-4xl text-center rounded-xl max-w-40" type="number">
+                    <input v-model.number="newBet" class="bg-secondary text-4xl text-center rounded-xl max-w-40" type="number">
                     <Button :disabled="invalidBet" class="text-4xl h-20 w-48" variant="secondary" @click="newGame">Place Bet</Button>
                 </div>
             </div>
@@ -69,29 +69,6 @@ const newGame = async () => {
         await game.placeBetAndStartNewGame(newBet.value);
     }
 }
-
-const statusTextClass = ref("");
-watchEffect(() => {
-    if (game.gameStatus.value === GameStatus.finished) {
-        statusTextClass.value = "text-white";
-    }
-    else {
-        statusTextClass.value = "text-primary";
-    }
-})
-
-const dealerStatusClasses = ref("bg-secondary text-primary");
-// watchEffect(() => {
-//     if (game.dealerBusted.value || game.currentGame.value?.result === "win") {
-//         dealerStatusClasses.value = "bg-destructive-foreground text-white";
-//     }
-//     else if (game.currentGame.value?.result === "lose" || game.currentGame.value?.result === "push") {
-//         dealerStatusClasses.value = "bg-affirmative text-white";
-//     }
-//     else {
-//         dealerStatusClasses.value = "bg-secondary text-primary";
-//     }
-// });
 
 const playerStatusClasses = ref("bg-secondary text-primary");
 watchEffect(() => {
